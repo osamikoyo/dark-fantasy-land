b41dfe2e691d9d408e187815a7282da2aa6d8472
Osamikoyo <v.pylayev@mail.ru>
2025-07-05 15:27:41 +0500

    update new entity

--- internal/entity/new.go
+++ internal/entity/new.go
@@ -1,8 +1,8 @@
 package entity
 
-type New struct{
-	Title string `bson:"title"`
-	Topic string `bson:"topic"`
-	Censor uint8 `bson:"censor"`
+type New struct {
+	Title   string `bson:"title"`
+	Topic   string `bson:"topic"`
+	Censor  uint8  `bson:"censor"`
 	Content string `bson:"content"`
 }
