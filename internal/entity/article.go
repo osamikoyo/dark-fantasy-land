b9849593b92cfe18e53183177cfd4189b0b18ffd
Osamikoyo <v.pylayev@mail.ru>
2025-07-05 09:01:10 +0500

    add entity article

---
+++ internal/entity/article.go
@@ -0,0 +1,10 @@
+package entity
+
+import "time"
+
+type Article struct{
+	Topic string `bson:"topic"`
+	Timestamp time.Time `bson:"timestamp"`
+	Content string `bson:"content"`
+	Author string `bson:"author"`
+}
