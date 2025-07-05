2440dfd8e61cbdaed81f75f09818e9dac461b399
Osamikoyo <v.pylayev@mail.ru>
2025-07-05 15:26:54 +0500

    add entity article

--- internal/entity/article.go
+++ internal/entity/article.go
@@ -2,9 +2,9 @@ package entity
 
 import "time"
 
-type Article struct{
-	Topic string `bson:"topic"`
+type Article struct {
+	Topic     string    `bson:"topic"`
 	Timestamp time.Time `bson:"timestamp"`
-	Content string `bson:"content"`
-	Author string `bson:"author"`
+	Content   string    `bson:"content"`
+	Author    string    `bson:"author"`
 }
