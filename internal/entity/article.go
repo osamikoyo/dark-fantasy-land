2cdc5ecc6bd1725006d128967740925e26aa3d82
Osamikoyo <v.pylayev@mail.ru>
2025-07-06 08:25:31 +0500

    update entity article

--- internal/entity/article.go
+++ internal/entity/article.go
@@ -3,7 +3,8 @@ package entity
 import "time"
 
 type Article struct {
-	Topic     string    `bson:"topic"`
+	Title string `bson:"title"`
+	Topics     []string    `bson:"topics"`
 	Timestamp time.Time `bson:"timestamp"`
 	Content   string    `bson:"content"`
 	Author    string    `bson:"author"`
