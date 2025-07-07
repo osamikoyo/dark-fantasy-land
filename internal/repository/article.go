349b9e67aeba528c7a6a0bdbcea4c6ad79e16cbe
Osamikoyo <v.pylayev@mail.ru>
2025-07-07 09:09:59 +0500

    update article repository

--- internal/repository/article.go
+++ internal/repository/article.go
@@ -24,3 +24,75 @@ func (r *Repository) CreateArticle(ctx context.Context, article *entity.Article)
 
 	return nil
 }
+
+func (r *Repository) UpdateArticle(ctx context.Context, filter map[string]string, update map[string]string) error {
+	r.logger.Debug("updating article",
+		zap.Any("filter", filter),
+		zap.Any("update", update))
+
+	res, err := r.articlesColl.UpdateOne(ctx, filter, update)
+	if err != nil {
+		r.logger.Error("failed update article", zap.Error(err))
+
+		return err
+	}
+
+	r.logger.Info("article updated",
+		zap.Int64("modifed_count", res.ModifiedCount))
+
+	return nil
+}
+
+func (r *Repository) DeleteArticle(ctx context.Context, filter map[string]string) error {
+	r.logger.Debug("deleting article",
+		zap.Any("filter", filter))
+
+	_, err := r.articlesColl.DeleteOne(ctx, filter)
+	if err != nil {
+		r.logger.Error("failed delete article",
+			zap.Error(err))
+
+		return err
+	}
+
+	r.logger.Info("deleted article",
+		zap.Any("filter", filter))
+
+	return nil
+}
+
+func (r *Repository) GetArticle(ctx context.Context, filter map[string]string) ([]entity.Article, error) {
+	r.logger.Debug("fetching article",
+		zap.Any("filter", filter))
+
+	res, err := r.articlesColl.Find(ctx, filter)
+	if err != nil {
+		r.logger.Error("failed fetch articles",
+			zap.Any("filter", filter),
+			zap.Error(err))
+
+		return nil, err
+	}
+
+	var articles []entity.Article
+
+	for res.Next(ctx) {
+		var article entity.Article
+
+		if err = res.Decode(&article); err != nil {
+			r.logger.Warn("failed decode article", zap.Error(err))
+
+			continue
+		}
+
+		articles = append(articles, article)
+	}
+
+	if err = res.Err(); err != nil {
+		r.logger.Error("error from fetch response", zap.Error(err))
+
+		return nil, err
+	}
+
+	return articles, nil
+}
