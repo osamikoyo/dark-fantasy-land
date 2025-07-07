a43752fefde7c8a87c1b899bc5350717c592b359
Osamikoyo <v.pylayev@mail.ru>
2025-07-07 09:09:46 +0500

    update entity mems

--- internal/entity/mems.go
+++ internal/entity/mems.go
@@ -2,10 +2,10 @@ package entity
 
 import "time"
 
-type Mem struct{
-	ImageName string `bson:"image_name"`
-	Topics []string `bson:"topics"`
-	Author string `bson:"author"`
-	Timestamp time.Time `bson:"timestamp"`
-	Description string `bson:"description"`
+type Mem struct {
+	ImageName   string    `bson:"image_name"`
+	Topics      []string  `bson:"topics"`
+	Author      string    `bson:"author"`
+	Timestamp   time.Time `bson:"timestamp"`
+	Description string    `bson:"description"`
 }
