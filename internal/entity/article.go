b7181ec9ad16e758f1c50243450dbc611e441861
Osamikoyo <v.pylayev@mail.ru>
2025-07-08 08:44:39 +0500

    update entity article

--- internal/entity/article.go
+++ internal/entity/article.go
@@ -3,9 +3,9 @@ package entity
 import "time"
 
 type Article struct {
-	Title     string    `bson:"title"`
-	Topics    []string  `bson:"topics"`
-	Timestamp time.Time `bson:"timestamp"`
-	Content   string    `bson:"content"`
-	Author    string    `bson:"author"`
+	Title     string    `bson:"title" redis:"title" mapstructure:"title"`
+	Topics    []string  `bson:"topics" redis:"topics" mapstructure:"topics"`
+	Timestamp time.Time `bson:"timestamp" redis:"timestamp" mapstructure:"timestamp"`
+	Content   string    `bson:"content" redis:"content" mapstructure:"content"`
+	Author    string    `bson:"author" redis:"author" mapstructure:"author"`
 }
