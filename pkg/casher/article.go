12c62f85a1045a9316eba8a1d9bd9ce23bd12d3c
Osamikoyo <v.pylayev@mail.ru>
2025-07-09 09:02:18 +0500

    update article casher

--- pkg/casher/article.go
+++ pkg/casher/article.go
@@ -30,11 +30,13 @@ func (c *Casher) AddArticleToCash(ctx context.Context, article *entity.Article) 
 }
 
 func (c *Casher) GetArticleFromCash(ctx context.Context, author, title string) (*entity.Article, error) {
-	if author == "" && title == "" {
+	if author == "" || title == "" {
 		return nil, NIL_INPUT_ERROR
 	}
 
 	key := newArticleKey(author, title)
+
+	c.logger.Debug("fetching article", zap.String("key", key))
 
 	res, err := c.client.HGetAll(ctx, key).Result()
 	if err != nil {
@@ -56,4 +58,23 @@ func (c *Casher) GetArticleFromCash(ctx context.Context, author, title string) (
 	}
 
 	return &article, nil
+}
+
+func (c *Casher) UpdateArticleInCash(ctx context.Context, author, title, key string, value interface{}) error {
+	if author == "" || title == "" || key == "" {
+		return NIL_INPUT_ERROR
+	}
+
+	redisKey := newArticleKey(author, title)
+
+	_, err := c.client.HSet(ctx, redisKey, key, value).Result()
+	if err != nil {
+		c.logger.Error("failed update article in hash",
+			zap.String("key", key),
+			zap.Error(err))
+
+		return err
+	}
+
+	return nil                                  
 }
