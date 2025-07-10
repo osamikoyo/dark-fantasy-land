b5df87a69cfe062c8f01352396b473b6f8c5962c
Osamikoyo <v.pylayev@mail.ru>
2025-07-10 10:23:18 +0500

    update new repository

--- internal/repository/new.go
+++ internal/repository/new.go
@@ -2,89 +2,126 @@ package repository
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/osamikoyo/dark-fantasy-land/internal/entity"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.uber.org/zap"
 )
 
 func (r *Repository) CreateNew(ctx context.Context, New *entity.New) error {
 	res, err := r.newsColl.InsertOne(ctx, New)
 	if err != nil {
-		r.logger.Error("failed create new",
-			zap.Error(err))
-
-		return err
+		r.logger.Error("failed create new", zap.Error(err))
+		return fmt.Errorf("create new: %w", ErrInsertFailed)
 	}
 
 	r.logger.Info("new created",
 		zap.String("title", New.Title),
-		zap.String("inserted_id", res.InsertedID.(string)))
+		zap.String("inserted_id", fmt.Sprintf("%v", res.InsertedID)))
 
 	return nil
 }
 
-func (r *Repository) UpdateNew(ctx context.Context, filter map[string]string, update map[string]string) error {
-	r.logger.Debug("updating new",
-		zap.Any("filter", filter),
-		zap.Any("update", update))
+func (r *Repository) UpdateNew(ctx context.Context, filter, update map[string]interface{}) error {
+	r.logger.Debug("updating new", zap.Any("filter", filter), zap.Any("update", update))
 
 	res, err := r.newsColl.UpdateOne(ctx, filter, update)
 	if err != nil {
 		r.logger.Error("failed update one", zap.Error(err))
-		return err
+		return fmt.Errorf("update new: %w", ErrUpdateFailed)
 	}
 
-	r.logger.Info("update one new",
-		zap.Int64("matched_count", res.MatchedCount))
+	if res.MatchedCount == 0 {
+		return ErrNotFound
+	}
+
+	r.logger.Info("update one new", zap.Int64("matched_count", res.MatchedCount))
 
 	return nil
 }
 
-func (r *Repository) GetNews(ctx context.Context, filter map[string]string) ([]entity.New, error) {
+func (r *Repository) GetNews(ctx context.Context, filter map[string]interface{}) ([]entity.New, error) {
 	r.logger.Debug("fetching news", zap.Any("filter", filter))
 
 	res, err := r.newsColl.Find(ctx, filter)
 	if err != nil {
 		r.logger.Error("failed get news", zap.Error(err))
-
-		return nil, err
+		return nil, fmt.Errorf("get news: %w", ErrNotFound)
 	}
-
 	defer res.Close(ctx)
 
 	var news []entity.New
 
 	for res.Next(ctx) {
 		var new entity.New
-
 		if err := res.Decode(&new); err != nil {
 			r.logger.Warn("failed decode new", zap.Error(err))
-			continue
+			return nil, fmt.Errorf("decode news: %w", ErrDecodeFailed)
 		}
-
 		news = append(news, new)
 	}
 
 	if err = res.Err(); err != nil {
 		r.logger.Error("failed parse news", zap.Error(err))
-
-		return nil, err
+		return nil, fmt.Errorf("parse news: %w", ErrDecodeFailed)
 	}
 
-	r.logger.Info("news was got",
-		zap.Int("length", len(news)))
+	if len(news) == 0 {
+		return nil, ErrNoDocuments
+	}
+
+	r.logger.Info("news was got", zap.Int("length", len(news)))
 
 	return news, nil
 }
 
-func (r *Repository) DeleteNew(ctx context.Context, filter map[string]string) error {
+func (r *Repository) GetNewsLimited(ctx context.Context, filter map[string]interface{}, limit int64) ([]entity.New, error) {
+	r.logger.Debug("fetching limited news", zap.Any("filter", filter), zap.Int64("limit", limit))
+
+	findOptions := options.Find()
+	findOptions.SetLimit(limit)
+
+	res, err := r.newsColl.Find(ctx, filter, findOptions)
+	if err != nil {
+		r.logger.Error("failed get limited news", zap.Error(err))
+		return nil, fmt.Errorf("get limited news: %w", ErrNotFound)
+	}
+	defer res.Close(ctx)
+
+	var news []entity.New
+	for res.Next(ctx) {
+		var n entity.New
+		if err := res.Decode(&n); err != nil {
+			r.logger.Warn("failed decode new", zap.Error(err))
+			return nil, fmt.Errorf("decode news: %w", ErrDecodeFailed)
+		}
+		news = append(news, n)
+	}
+	if err = res.Err(); err != nil {
+		r.logger.Error("failed parse news", zap.Error(err))
+		return nil, fmt.Errorf("parse news: %w", ErrDecodeFailed)
+	}
+
+	if len(news) == 0 {
+		return nil, ErrNoDocuments
+	}
+
+	r.logger.Info("news fetched (limited)", zap.Int("length", len(news)))
+	return news, nil
+}
+
+func (r *Repository) DeleteNew(ctx context.Context, filter map[string]interface{}) error {
 	r.logger.Debug("deleting new", zap.Any("filter", filter))
 
-	_, err := r.newsColl.DeleteOne(ctx, filter)
+	res, err := r.newsColl.DeleteOne(ctx, filter)
 	if err != nil {
 		r.logger.Error("failed delete new", zap.Error(err))
+		return fmt.Errorf("delete new: %w", ErrDeleteFailed)
+	}
 
-		return err
+	if res.DeletedCount == 0 {
+		return ErrNotFound
 	}
 
 	r.logger.Info("new deleted", zap.Any("filter", filter))
