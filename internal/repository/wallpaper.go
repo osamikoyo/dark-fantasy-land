587f08774d8ea1bfd9def16a5fa87a5a1e0747fa
Osamikoyo <v.pylayev@mail.ru>
2025-07-10 10:23:35 +0500

    update wallpaper repository

--- internal/repository/wallpaper.go
+++ internal/repository/wallpaper.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/osamikoyo/dark-fantasy-land/internal/entity"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.uber.org/zap"
 )
 
@@ -20,7 +21,7 @@ func (r *Repository) CreateWallpaper(ctx context.Context, wallpaper *entity.Wall
 	return nil
 }
 
-func (r *Repository) UpdateWallpaper(ctx context.Context, filter map[string]string, update map[string]string) error {
+func (r *Repository) UpdateWallpaper(ctx context.Context, filter, update map[string]interface{}) error {
 	r.logger.Debug("updating wallpaper", zap.Any("filter", filter), zap.Any("update", update))
 	res, err := r.wallpaperColl.UpdateOne(ctx, filter, update)
 	if err != nil {
@@ -32,7 +33,7 @@ func (r *Repository) UpdateWallpaper(ctx context.Context, filter map[string]stri
 	return nil
 }
 
-func (r *Repository) GetWallpapers(ctx context.Context, filter map[string]string) ([]entity.Wallpaper, error) {
+func (r *Repository) GetWallpapers(ctx context.Context, filter map[string]interface{}) ([]entity.Wallpaper, error) {
 	r.logger.Debug("fetching wallpapers", zap.Any("filter", filter))
 	res, err := r.wallpaperColl.Find(ctx, filter)
 	if err != nil {
@@ -59,7 +60,38 @@ func (r *Repository) GetWallpapers(ctx context.Context, filter map[string]string
 	return wallpapers, nil
 }
 
-func (r *Repository) DeleteWallpaper(ctx context.Context, filter map[string]string) error {
+func (r *Repository) GetWallpapersLimited(ctx context.Context, filter map[string]interface{}, limit int64) ([]entity.Wallpaper, error) {
+	r.logger.Debug("fetching limited wallpapers", zap.Any("filter", filter), zap.Int64("limit", limit))
+
+	findOptions := options.Find()
+	findOptions.SetLimit(limit)
+
+	res, err := r.wallpaperColl.Find(ctx, filter, findOptions)
+	if err != nil {
+		r.logger.Error("failed to get limited wallpapers", zap.Error(err))
+		return nil, err
+	}
+	defer res.Close(ctx)
+
+	var wallpapers []entity.Wallpaper
+	for res.Next(ctx) {
+		var wallpaper entity.Wallpaper
+		if err := res.Decode(&wallpaper); err != nil {
+			r.logger.Warn("failed to decode wallpaper", zap.Error(err))
+			continue
+		}
+		wallpapers = append(wallpapers, wallpaper)
+	}
+	if err = res.Err(); err != nil {
+		r.logger.Error("failed to parse wallpapers", zap.Error(err))
+		return nil, err
+	}
+
+	r.logger.Info("wallpapers fetched (limited)", zap.Int("length", len(wallpapers)))
+	return wallpapers, nil
+}
+
+func (r *Repository) DeleteWallpaper(ctx context.Context, filter map[string]interface{}) error {
 	r.logger.Debug("deleting wallpaper", zap.Any("filter", filter))
 	_, err := r.wallpaperColl.DeleteOne(ctx, filter)
 	if err != nil {
