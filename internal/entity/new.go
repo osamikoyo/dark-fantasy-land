776bb2e1e680f14d264123c449fd057e5bdfaac0
Osamikoyo <v.pylayev@mail.ru>
2025-07-10 10:22:38 +0500

    update entity new

--- internal/entity/new.go
+++ internal/entity/new.go
@@ -1,8 +1,12 @@
 package entity
 
+import "time"
+
 type New struct {
-	Title   string `bson:"title"`
-	Topic   string `bson:"topic"`
-	Censor  uint8  `bson:"censor"`
-	Content string `bson:"content"`
+	Title     string    `bson:"title"`
+	Topic     string    `bson:"topic"`
+	Author    string    `bson:"author"`
+	Censor    uint8     `bson:"censor"`
+	Content   string    `bson:"content"`
+	Timestamp time.Time `bson:"timestamp"`
 }
