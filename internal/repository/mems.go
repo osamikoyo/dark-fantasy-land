72d767ba03894d25c844b0ba6c47e4e6accf6c09
Osamikoyo <v.pylayev@mail.ru>
2025-07-10 10:23:04 +0500

    update mems repository

--- internal/repository/mems.go
+++ internal/repository/mems.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/osamikoyo/dark-fantasy-land/internal/entity"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.uber.org/zap"
 )
 
@@ -20,7 +21,7 @@ func (r *Repository) CreateMem(ctx context.Context, mem *entity.Mem) error {
 	return nil
 }
 
-func (r *Repository) UpdateMem(ctx context.Context, filter map[string]string, update map[string]string) error {
+func (r *Repository) UpdateMem(ctx context.Context, filter, update map[string]interface{}) error {
 	r.logger.Debug("updating mem", zap.Any("filter", filter), zap.Any("update", update))
 	res, err := r.cfuColl.UpdateOne(ctx, filter, update)
 	if err != nil {
@@ -32,7 +33,7 @@ func (r *Repository) UpdateMem(ctx context.Context, filter map[string]string, up
 	return nil
 }
 
-func (r *Repository) GetMems(ctx context.Context, filter map[string]string) ([]entity.Mem, error) {
+func (r *Repository) GetMems(ctx context.Context, filter map[string]interface{}) ([]entity.Mem, error) {
 	r.logger.Debug("fetching mems", zap.Any("filter", filter))
 	res, err := r.cfuColl.Find(ctx, filter)
 	if err != nil {
@@ -59,7 +60,38 @@ func (r *Repository) GetMems(ctx context.Context, filter map[string]string) ([]e
 	return mems, nil
 }
 
-func (r *Repository) DeleteMem(ctx context.Context, filter map[string]string) error {
+func (r *Repository) GetMemsLimited(ctx context.Context, filter map[string]interface{}, limit int64) ([]entity.Mem, error) {
+	r.logger.Debug("fetching limited mems", zap.Any("filter", filter), zap.Int64("limit", limit))
+
+	findOptions := options.Find()
+	findOptions.SetLimit(limit)
+
+	res, err := r.cfuColl.Find(ctx, filter, findOptions)
+	if err != nil {
+		r.logger.Error("failed to get limited mems", zap.Error(err))
+		return nil, err
+	}
+	defer res.Close(ctx)
+
+	var mems []entity.Mem
+	for res.Next(ctx) {
+		var mem entity.Mem
+		if err := res.Decode(&mem); err != nil {
+			r.logger.Warn("failed to decode mem", zap.Error(err))
+			continue
+		}
+		mems = append(mems, mem)
+	}
+	if err = res.Err(); err != nil {
+		r.logger.Error("failed to parse mems", zap.Error(err))
+		return nil, err
+	}
+
+	r.logger.Info("mems fetched (limited)", zap.Int("length", len(mems)))
+	return mems, nil
+}
+
+func (r *Repository) DeleteMem(ctx context.Context, filter map[string]interface{}) error {
 	r.logger.Debug("deleting mem", zap.Any("filter", filter))
 	_, err := r.cfuColl.DeleteOne(ctx, filter)
 	if err != nil {
