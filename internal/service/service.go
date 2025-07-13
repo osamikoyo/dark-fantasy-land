12071574843a9c45d9b5c4ebda990adc8e024628
Osamikoyo <v.pylayev@mail.ru>
2025-07-13 08:38:28 +0500

    update service

--- internal/service/service.go
+++ internal/service/service.go
@@ -2,26 +2,36 @@ package service
 
 import (
 	"context"
+	"errors"
 	"time"
 )
 
 var (
-	
+	ErrNotFound         = errors.New("not found")
+	ErrTimeout          = errors.New("operation timeout")
+	ErrInvalidInput     = errors.New("invalid input")
+	ErrAlreadyExists    = errors.New("already exist")
+	ErrCacheSetFailed   = errors.New("cache set failed")
+	ErrCacheGetFailed   = errors.New("cache get failed")
+	ErrCacheDelFailed   = errors.New("cache delete failed")
+	ErrRepositoryFailed = errors.New("repository operation failed")
+	ErrInternal         = errors.New("internal service error")
 )
 
 type (
 	Service struct {
 		repo   Repository
 		casher Casher
+		sender Sender
 
 		timeout time.Duration
 	}
 )
 
-func NewService(repo Repository, casher Casher, timeout time.Duration) *Service {
+func NewService(repo Repository, casher Casher, sender Sender, timeout time.Duration) *Service {
 	return &Service{
-		repo: repo,
-		casher: casher,
+		repo:    repo,
+		casher:  casher,
 		timeout: timeout,
 	}
 }
@@ -29,4 +39,3 @@ func NewService(repo Repository, casher Casher, timeout time.Duration) *Service 
 func (s *Service) context() (context.Context, context.CancelFunc) {
 	return context.WithTimeout(context.Background(), s.timeout)
 }
-
