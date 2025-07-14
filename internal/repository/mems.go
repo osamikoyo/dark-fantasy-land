5886322ce1c301124e58e08c3c2a17fe267588f5
Osamikoyo <v.pylayev@mail.ru>
2025-07-14 08:33:45 +0500

    update mems repository

--- internal/repository/mems.go
+++ internal/repository/mems.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/osamikoyo/dark-fantasy-land/internal/entity"
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.uber.org/zap"
 )
@@ -36,37 +37,6 @@ func (r *Repository) UpdateMem(ctx context.Context, filter, update map[string]in
 
 	r.logger.Info("mem updated", zap.Int64("matched_count", res.MatchedCount))
 	return nil
-}
-
-func (r *Repository) GetMems(ctx context.Context, filter map[string]interface{}) ([]entity.Mem, error) {
-	r.logger.Debug("fetching mems", zap.Any("filter", filter))
-	res, err := r.cfuColl.Find(ctx, filter)
-	if err != nil {
-		r.logger.Error("failed to get mems", zap.Error(err))
-		return nil, fmt.Errorf("get mems: %w", ErrNotFound)
-	}
-	defer res.Close(ctx)
-
-	var mems []entity.Mem
-	for res.Next(ctx) {
-		var mem entity.Mem
-		if err := res.Decode(&mem); err != nil {
-			r.logger.Warn("failed to decode mem", zap.Error(err))
-			return nil, fmt.Errorf("decode mem: %w", ErrDecodeFailed)
-		}
-		mems = append(mems, mem)
-	}
-	if err = res.Err(); err != nil {
-		r.logger.Error("failed to parse mems", zap.Error(err))
-		return nil, fmt.Errorf("parse mems: %w", ErrDecodeFailed)
-	}
-
-	if len(mems) == 0 {
-		return nil, ErrNoDocuments
-	}
-
-	r.logger.Info("mems fetched", zap.Int("length", len(mems)))
-	return mems, nil
 }
 
 func (r *Repository) GetMemsLimited(ctx context.Context, filter map[string]interface{}, limit int64) ([]entity.Mem, error) {
@@ -118,4 +88,27 @@ func (r *Repository) DeleteMem(ctx context.Context, filter map[string]interface{
 
 	r.logger.Info("mem deleted", zap.Any("filter", filter))
 	return nil
+}
+
+func (r *Repository) GetMem(ctx context.Context, filter map[string]interface{}) (*entity.Mem, error) {
+	r.logger.Debug("fetching single mem", zap.Any("filter", filter))
+
+	res := r.cfuColl.FindOne(ctx, filter)
+	if res.Err() != nil {
+		if res.Err() == mongo.ErrNoDocuments {
+			r.logger.Warn("mem not found", zap.Any("filter", filter))
+			return nil, ErrNotFound
+		}
+		r.logger.Error("failed to get mem", zap.Error(res.Err()))
+		return nil, fmt.Errorf("get mem: %w", res.Err())
+	}
+
+	var mem entity.Mem
+	if err := res.Decode(&mem); err != nil {
+		r.logger.Warn("failed to decode mem", zap.Error(err))
+		return nil, fmt.Errorf("decode mem: %w", ErrDecodeFailed)
+	}
+
+	r.logger.Info("mem fetched", zap.Any("mem", mem))
+	return &mem, nil
 }
