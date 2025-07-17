e2a267bb1dd64fbc0cb8bbfda97ea69a06ed1e58
Osamikoyo <v.pylayev@mail.ru>
2025-07-17 09:40:15 +0500

    update config

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,15 +3,15 @@ package config
 import "os"
 
 type Config struct {
-	Port     string
-	Host     string
-	MongoUrl string
-	RedisUrl string
-	MinioUrl string
+	Port           string
+	Host           string
+	MongoUrl       string
+	RedisUrl       string
+	MinioUrl       string
 	MinioAccessKey string
 	MinioSecretKey string
-	MinioBuckets [2]string
-	MinioSSL bool
+	MinioBuckets   []string
+	MinioSSL       bool
 }
 
 func NewConfig() *Config {
@@ -26,14 +26,14 @@ func NewConfig() *Config {
 	}
 
 	return &Config{
-		Port:     "8080",
-		Host:     "localhost",
-		MongoUrl: url,
-		RedisUrl: redisUrl,
-		MinioUrl: "localhost:9000",
+		Port:           "8080",
+		Host:           "localhost",
+		MongoUrl:       url,
+		RedisUrl:       redisUrl,
+		MinioUrl:       "localhost:9000",
 		MinioAccessKey: "minioadmin",
 		MinioSecretKey: "minioadmin",
-		MinioBuckets: [2]string{
+		MinioBuckets: []string{
 			"wallpapers",
 			"mems",
 		},
