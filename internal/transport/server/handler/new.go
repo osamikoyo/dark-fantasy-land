c39a2944ba0f8d3c410367c54824c9b0aea1b602
Osamikoyo <v.pylayev@mail.ru>
2025-07-23 09:19:01 +0500

    add new handler

---
+++ internal/transport/server/handler/new.go
@@ -0,0 +1,56 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/osamikoyo/dark-fantasy-land/internal/entity"
+)
+
+func (h *Handler) CreateNew(c echo.Context) error {
+	var new entity.New
+
+	if err := c.Bind(&new); err != nil {
+		return c.String(http.StatusBadRequest, ErrInvalidInput)
+	}
+
+	if err := h.service.CreateNew(&new); err != nil {
+		return c.String(http.StatusInternalServerError, ErrInternal)
+	}
+
+	return c.String(http.StatusCreated, "new created")
+}
+
+func (h *Handler) GetNew(c echo.Context) error {
+	author := c.Param("author")
+	title := c.Param("title")
+
+	new, err := h.service.GetOneNew(author, title)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, ErrInternal)
+	}
+
+	return c.JSON(http.StatusOK, new)
+}
+
+func (h *Handler) GetNews(c echo.Context) error {
+	params := []string{"author", "title", "timestamp", "content"}
+
+	filter := make(map[string]interface{})
+	for _, p := range params {
+		filter[p] = c.Param(p)
+	}
+
+	for key, value := range filter {
+		if value == "" {
+			delete(filter, key)
+		}
+	}
+
+	news, err := h.service.GetManyNew(filter)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, ErrInternal)
+	}
+
+	return c.JSON(http.StatusOK, news)
+}
