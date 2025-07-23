636f6c4c1d00fb88d1e1a20ed7f9021474fc45da
Osamikoyo <v.pylayev@mail.ru>
2025-07-23 09:19:22 +0500

    add wallpaper handler

---
+++ internal/transport/server/handler/wallpaper.go
@@ -0,0 +1,91 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/osamikoyo/dark-fantasy-land/internal/entity"
+)
+
+func (h *Handler) CreateWallpaper(c echo.Context) error {
+	var wallpaper entity.Wallpaper
+
+	if err := c.Bind(&wallpaper); err != nil {
+		return c.String(http.StatusBadRequest, ErrInvalidInput)
+	}
+
+	file, err := c.FormFile("image")
+	if err != nil {
+		return c.String(http.StatusBadRequest, ErrInvalidInput)
+	}
+
+	if err = h.storage.UploadFile(file, h.cfg.MinioBuckets.WallpaperFull); err != nil {
+		return c.String(http.StatusInternalServerError, ErrInternal)
+	}
+
+	if err = h.storage.UploadAndCommpress(file, h.cfg.MinioBuckets.WallpaperWatch); err != nil {
+		return c.String(http.StatusInternalServerError, ErrInternal)
+	}
+
+	if err = h.service.CreateWallpaper(&wallpaper); err != nil {
+		return c.String(http.StatusInternalServerError, ErrInternal)
+	}
+
+	return c.String(http.StatusCreated, "wallpaper created")
+}
+
+func (h *Handler) GetWallpapers(c echo.Context) error {
+	params := []string{"author", "title", "image_name", "timestamp"}
+
+	filter := make(map[string]interface{})
+	for _, p := range params {
+		filter[p] = c.Param(p)
+	}
+
+	for key, value := range filter {
+		if value == "" {
+			delete(filter, key)
+		}
+	}
+
+	wallpapers, err := h.service.GetManyWallpapers(filter)
+	if err != nil{
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+
+	return c.JSON(http.StatusOK, wallpapers)
+}
+
+func (h *Handler) GetWallpaperInfo(c echo.Context) error {
+	title := c.Param("title")
+	image_name := c.Param("image_name")
+
+	wallpaper, err := h.service.GetOneWallpaper(image_name, title)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, ErrInternal)
+	}
+
+	return c.JSON(http.StatusOK, wallpaper)
+}
+
+func (h *Handler) GetWallpaperImage(c echo.Context) error {
+	image_name := c.Param("image_name")
+
+	wallpaper, err := h.storage.DownloadFile(image_name, h.cfg.MinioBuckets.WallpaperWatch)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, ErrInternal)
+	}
+
+	return c.Stream(http.StatusOK, "application/octet-stream", wallpaper)
+}
+
+func (h *Handler) DownloadWallpaper(c echo.Context) error {
+	image_name := c.Param("image_name")
+
+	wallpaper, err := h.storage.DownloadFile(image_name, h.cfg.MinioBuckets.WallpaperFull)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, ErrInternal)
+	}
+
+	return c.Stream(http.StatusOK, "application/octet-stream", wallpaper)
+}
